ef5c74f7b77bd556829e7314120c6e20e64d4698
FayJu <faizan@unetiq.com>
2022-05-29 18:53:27 +0200

    added token

--- classes/customer.py
+++ classes/customer.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta
 import sqlite3
 from sqlite3 import Error
 
+import jwt
+
 
 class Customer:
+
     def __init__(self, email):
         self.email = email
         self.first_name = None
@@ -78,6 +82,29 @@ class Customer:
             "phone": self.phone,
             "email": self.email,
         }
+
+    def get_customer_token(self, secret):
+        customer_data = self.get_customer_details()
+        customer_data['exp'] = datetime.now() + timedelta(seconds=20)
+        return jwt.encode(customer_data, secret, algorithm='HS256')
+
+    def is_valid_token(self, token, secret):
+        try:
+            jwt.decode(token, secret, algorithms=['HS256'])
+            return True
+        except Error as e:
+            print(f"The error '{e}' occurred")
+            return False
+
+    def set_customer_data_from_token(self, token, secret):
+        customer_data = jwt.decode(token, secret, algorithms=['HS256'])
+        self.first_name = customer_data['firstName']
+        self.last_name = customer_data['lastName']
+        self.address = customer_data['address']
+        self.phone = customer_data['phone']
+        self.email = customer_data['email']
+
+        return self.get_customer_details()
 
     def __repr__(self):
         return f"<Customer {self.first_name}>"
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, Header
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from classes.customer import Customer
@@ -9,7 +9,8 @@ from db.database import Database
 
 load_dotenv()
 origins = [os.environ.get("front-end")]
-print(os.environ.get("front-end"))
+
+secret = os.environ.get("jwt-secret")
 
 app = FastAPI()
 
@@ -46,7 +47,12 @@ async def create_customer(user: RegisterUser, response: Response):
     if customer.is_new_customer():
         customer.register_customer(
             user.first_name, user.last_name, user.address, user.phone, user.password)
-        return {"message": "Your account is successfully created."}
+
+        return {
+            "message": "Your account is successfully created.",
+            "customer": customer.get_customer_details(),
+            "token": customer.get_customer_token(secret)
+        }
     else:
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"message": "Your email is already registered."}
@@ -61,7 +67,24 @@ class LoginUser(BaseModel):
 async def login_customer(user: LoginUser, response: Response):
     customer = Customer(user.email)
     if customer.login_customer(user.password):
-        return {"message": "You are successfully logged in.", "customer": customer.get_customer_details()}
+        return {
+            "message": "You are successfully logged in.",
+            "customer": customer.get_customer_details(),
+            "token": customer.get_customer_token(secret)
+        }
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return {"message": "Invalid credentials."}
+
+
+@app.get("/customer/data", status_code=status.HTTP_200_OK)
+async def get_customer_data(token: str = Header(None)):
+    customer = Customer('')
+    if customer.is_valid_token(token, secret):
+        customer.set_customer_data_from_token(token, secret)
+        return {
+            "message": "You are successfully logged in.",
+            "customer": customer.get_customer_details()
+        }
+    else:
+        return {"message": "Invalid token."}
